ea8c7d6a51c4ac068fce94b6633c1de44245626f
vavan19911987 <v.pulin@yahoo.com>
2020-11-22 20:41:35 +0300

    сделал header под все размеры

---
+++ src/js/jquery.js
@@ -0,0 +1,23 @@
+// $(document).ready(function () {
+//     var menuLink = $('.menu-link');
+//     var menu = $('menu');
+//     var close = $('.close-btn');
+//     var navLink = $('li a');
+
+//     menuLink.click(function () {
+//         menu.toggleClass('active-menu');
+//     });
+//     close.click(function () {
+//         menu.removeClass('active-menu');
+//     });
+
+//     navLink.on('click', function (event) {
+//         event.preventDefault();
+//         var target = $(this).attr('href');
+//         // console.log(target);
+//         var top = $(target).offset().top;
+//         // console.log(top);
+//         $('html,body').animate({scrollTop: top}, 500)
+        
+//     });
+// });
