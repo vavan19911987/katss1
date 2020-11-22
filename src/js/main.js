cd83f1b24187f0a0185a844c3dbbd7e992971e65
vavan19911987 <v.pulin@yahoo.com>
2020-11-22 15:25:15 +0300

    сделал кнопку вверх

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+var timeOut;
+function goUp() {
+   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+   if(top > 0) {
+      window.scrollBy(0,-100);
+      timeOut = setTimeout('goUp()',60);
+   } else clearTimeout(timeOut);
+} 
